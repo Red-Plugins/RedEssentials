4117729a0bd3ad184c613a9448897147ad5278ea
Yuri Silva <60571649+Neonzio@users.noreply.github.com>
2020-06-22 21:12:52 -0300

    Create PingListener.java

---
+++ src/me/neon/redessentials/listeners/PingListener.java
@@ -0,0 +1,13 @@
+package me.neon.redessentials.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.server.ServerListPingEvent;
+
+public class PingListener implements Listener {
+
+	@EventHandler
+	public void onServerPing(ServerListPingEvent event) {
+		event.setMotd("§cTest server!!");
+	}
+}
